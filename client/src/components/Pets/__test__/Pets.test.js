eaddd68a5c2e9bbdf011c5161e8578e9068b9311
Drew Peters <megamutha@gmail.com>
2022-10-16 14:53:54 -0400

    finished v46--use 'msw' to mock server for tests

--- client/src/components/Pets/__test__/Pets.test.js
+++ client/src/components/Pets/__test__/Pets.test.js
@@ -1,5 +1,24 @@
 import { render, screen } from "@testing-library/react"
 import Pets from "../Pets"
+import { rest } from "msw"
+import { setupServer } from "msw/node"
+import catsMock from "../../../mocks/cats.json"
+
+
+//here we can specify what we want to mock, what endpoint to mock, & what to return
+const server = setupServer(
+// http://localhost:4000/cats
+  rest.get("http://localhost:4000/cats", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(catsMock)
+    )
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 describe("Pets", () => {
   test("should render the correct amount of cards", async () => {
